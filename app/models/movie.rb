f78222a58715dbc0697991dc2cfd8826f2aa811a
Ryan Tan <skyfish81@gmail.com>
2017-12-12 01:40:05 +0800

    Add simple model validations

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,8 @@
 class Movie < ApplicationRecord
+    validates :title, presence: true
+    validates :year, allow_nil: true, numericality: 
+        { greater_than_or_equal_to: 1970, less_than_or_equal_to: 2500 }
+    validates :imdb_rate, allow_nil: true, numericality: 
+        { greater_than_or_equal_to: 0.0, less_than_or_equal_to: 10.0 }
+
 end
